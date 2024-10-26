669c7d2bfadc3ffca65164f62d2637bd83dff65b
Angus Gratton <gus@projectgus.com>
2024-10-26 11:49:18 +1100

    fw: Dump the error if DBC parsing fails.
    Goes from very terse (no details) to a full parse tree, but useful to find
    the place where it failed.

--- firmware/build.rs
+++ firmware/build.rs
@@ -10,6 +10,7 @@ fn main() {
     let config = Config::builder()
         .dbc_name("pcan.dbc")
         .dbc_content(&dbc_file)
+        .debug_prints(true)
         .allow_dead_code(true) // Don't emit warnings if not all generated code is used
         //.impl_arbitrary(FeatureConfig::Gated("arbitrary")) // Optional impls.
         .impl_debug(FeatureConfig::Always)
