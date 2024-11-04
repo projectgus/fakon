31c5e9f6ea92cacbeb31a29006b34b27cef6ed85
Angus Gratton <gus@projectgus.com>
2024-11-04 14:48:03 +1100

    WIP: fw: take a stab at making a PeriodicMessage trait, refactor IEB

--- firmware/src/can_utils.rs
+++ firmware/src/can_utils.rs
@@ -1,7 +1,5 @@
 // Some utility methods for CAN message contents
 
-use crate::dbc::pcan;
-
 // The simplest "checksum" of summing all bytes modulo 256
 pub fn byte_checksum_simple(data: &[u8]) -> u8 {
     data.iter().fold(0, |s,n| n.wrapping_add(s))
@@ -21,98 +19,5 @@ pub fn counter_update_skip<const MASK: u8, const SKIP: u8>(data: &mut u8) {
     counter_update::<MASK>(data);
     if *data & MASK == SKIP {
         counter_update::<MASK>(data);
-    }
-}
-
-/// Trait representing some common operations on a CAN message
-pub(crate) trait OutgoingMessage {
-    /// Increment the counter field(s) in the message
-    fn increment_counter(&mut self);
-
-    /// Update any checksum field(s) according to the contents of the message
-    fn update_checksum(&mut self);
-
-    /// Perform all regular "liveness" updates (counters, checksums)
-    #[inline]
-    fn update(&mut self) {
-        self.increment_counter();
-        self.update_checksum();
-    }
-}
-
-// Additional methods for StabilityControl message ID 0x220
-impl OutgoingMessage for pcan::StabilityControl {
-    #[inline]
-    fn increment_counter(&mut self) {
-        self.set_counter(match self.counter() {
-            Self::COUNTER_MAX.. => Self::COUNTER_MIN,
-            n => n + 1,
-        }).unwrap();
-    }
-
-    #[inline]
-    fn update_checksum(&mut self) {
-        self.set_checksum(0).unwrap();
-        let new_sum = self.raw().iter().fold(0u8, |n, e| n.wrapping_add(*e));
-        self.set_checksum((new_sum ^ 0x9) & 0xF).unwrap();
-    }
-}
-
-// Additional methods for TractionControlFast message ID 0x153
-impl OutgoingMessage for pcan::TractionControlFast {
-    #[inline]
-    fn increment_counter(&mut self) {
-                self
-                    .set_counter1(match self.counter1() {
-                        Self::COUNTER1_MAX.. => Self::COUNTER1_MIN,
-                        c => c + 1,
-                    }).unwrap();
-
-                self
-                    .set_counter2(match self.counter2() {
-                        Self::COUNTER2_MAX.. => Self::COUNTER2_MIN,
-                        0x8 => 0xA,  // counter2 skips 0x9 rather than 0xF
-                        c => c + 1,
-                    }).unwrap();
-    }
-
-    #[inline]
-    fn update_checksum(&mut self) {
-        // No checksum on this message
-    }
-}
-
-// Additional methods for TractionControlMed message ID 0x394
-impl OutgoingMessage for pcan::TractionControlMed {
-    fn increment_counter(&mut self) {
-        self.set_counter(match self.counter() {
-            Self::COUNTER_MAX.. => Self::COUNTER_MIN,
-            n => n + 1,
-        }).unwrap();
-    }
-
-    fn update_checksum(&mut self) {
-        self.set_checksum(0).unwrap();
-
-        // Checksum is the sum of the nibbles of all bytes except the last
-        let new_sum = self.raw()[..7].iter().fold(0u8, |n, e| {
-            (n + (e >> 4) + (e & 0xF)) & 0xF
-        });
-
-        // Then take binary complement, add one, and mask modulo 16
-        self.set_checksum(((new_sum ^ 0x0F) + 1) & 0x0F).unwrap();
-    }
-}
-
-impl OutgoingMessage for pcan::Unk471 {
-    fn increment_counter(&mut self) {
-        self.set_counter(match self.counter() {
-            Self::COUNTER_MAX.. => Self::COUNTER_MIN,
-            n => n + 1,
-        }).unwrap();
-    }
-
-    fn update_checksum(&mut self) {
-        // It's possible this message has a 2-bit checksum, but unclear
     }
 }
--- firmware/src/ieb.rs
+++ firmware/src/ieb.rs
@@ -3,172 +3,304 @@
 ///! Also manages traction control and vehicle stability control messages. Most of this
 ///! is spoofed, the VCU's perspective should be that it's forever driving in a straight
 ///! line down a road with perfect traction...
-use crate::can_queue;
-use crate::can_utils::{byte_checksum_simple, OutgoingMessage};
-use crate::car::{self, Ignition};
-use crate::dbc::pcan::{Ieb2a2, Ieb331, Ieb386Wheel, Ieb387Wheel, Ieb507Tcs, StabilityControl, TractionControlFast, TractionControlMed, ParkingBrake};
-use crate::hardware;
-use crate::periodic::PeriodicGroup;
+use crate::can_queue::{self, QueuedFrame};
+use crate::can_utils::byte_checksum_simple;
+use crate::car::{CarState, Ignition};
+use crate::dbc::pcan::{
+    Ieb2a2, Ieb331, Ieb386Wheel, Ieb387Wheel, Ieb507Tcs, ParkingBrake, StabilityControl,
+    TractionControlFast, TractionControlMed,
+};
+use crate::periodic::{send_periodic, PeriodicMessage};
+use crate::{hardware, Rate};
+use embedded_can::Frame;
 use fugit::RateExtU32;
 use hex_literal::hex;
 use rtic::Mutex;
 
-// TODO: Add the other brake pedal signal gpio
 pub async fn task_ieb<MPCAN, MCAR>(mut pcan_tx: MPCAN, mut car: MCAR) -> !
 where
     MPCAN: Mutex<T = can_queue::Tx<hardware::PCAN>>,
-    MCAR: Mutex<T = car::CarState>,
+    MCAR: Mutex<T = CarState>,
 {
-    let mut traction_fast = TractionControlFast::try_from(hex!("208010FF00FF40EE").as_slice()).unwrap();
-    let mut traction_med = TractionControlMed::try_from(hex!("00E00000FF43B298").as_slice()).unwrap();
+    // Initialise all the raw CAN messages
+
+    let mut traction_fast =
+        TractionControlFast::try_from(hex!("208010FF00FF40EE").as_slice()).unwrap();
+    let mut traction_med =
+        TractionControlMed::try_from(hex!("00E00000FF43B298").as_slice()).unwrap();
     let mut brake_pedal = Ieb2a2::try_from(hex!("0500001C1000005E").as_slice()).unwrap();
     let mut ieb_331 = Ieb331::try_from(hex!("F000000000000000").as_slice()).unwrap();
     let mut ieb_386 = Ieb386Wheel::try_from(hex!("0000000000400080").as_slice()).unwrap();
     let mut ieb_387 = Ieb387Wheel::try_from(hex!("0A0D000000210A00").as_slice()).unwrap();
-    let ieb_507 = Ieb507Tcs::try_from(hex!("00000001").as_slice()).unwrap();
+    let mut ieb_507 = Ieb507Tcs::try_from(hex!("00000001").as_slice()).unwrap();
     // Parking brake has a lot of fields but can send a static "parking brake off" message
-    let parking_brake = ParkingBrake::try_from(hex!("0000082100000000").as_slice()).unwrap();
-    let mut stability = StabilityControl::new(0.0, false, false, // Lat accel
-                                           0.0, false, false, // Long accel
-                                           0.0, false, false, // Cyl pressure
-                                           0.0, false, false, // Yaw rate
-                                           0, 0).unwrap();
+    let mut parking_brake = ParkingBrake::try_from(hex!("0000082100000000").as_slice()).unwrap();
+    let mut stability = StabilityControl::new(
+        0.0, false, false, // Lat accel
+        0.0, false, false, // Long accel
+        0.0, false, false, // Cyl pressure
+        0.0, false, false, // Yaw rate
+        0, 0,
+    )
+    .unwrap();
 
-    let mut group = PeriodicGroup::new(100.Hz());
-    let mut every_100hz = group.new_period(100.Hz());
-    let mut every_50hz = group.new_period(50.Hz());
-    let mut every_20hz = group.new_period(20.Hz());
-    let mut every_10hz = group.new_period(10.Hz());
+    send_periodic(
+        &mut [
+            &mut traction_fast,
+            &mut traction_med,
+            &mut stability,
+            &mut brake_pedal,
+            &mut ieb_331,
+            &mut parking_brake,
+            &mut ieb_386,
+            &mut ieb_387,
+            &mut ieb_507,
+        ],
+        &mut pcan_tx,
+        &mut car,
+    )
+    .await;
+}
 
-    loop {
-        group.next_poll().await;
+impl PeriodicMessage for TractionControlFast {
+    fn rate(&self) -> Rate {
+        100.Hz()
+    }
 
-        if !car.lock(|car| car.ignition() == Ignition::On) {
-            // IEB only starts sending messages when car ignition comes on.
-            //
-            // NOTE: It keeps sending them for a time after ignition goes off, until the
-            // thing goes to sleep. Unsure if need to model that here or not.
-            continue;
+    fn update_for_transmit(&mut self, car: &CarState) -> Option<QueuedFrame> {
+        if car.ignition() != Ignition::On {
+            return None; // IEB is only on when car is on
         }
 
-        // Read once per loop for consistency
-        let braking = car.lock(|car| car.is_braking());
+        // Update counters
+        {
+            self.set_counter1(match self.counter1() {
+                Self::COUNTER1_MAX.. => Self::COUNTER1_MIN,
+                c => c + 1,
+            })
+            .unwrap();
 
-        if every_100hz.due(&group) {
-            // Traction Control status ("fast" 100Hz message)
-            {
-                traction_fast.update();
+            self.set_counter2(match self.counter2() {
+                Self::COUNTER2_MAX.. => Self::COUNTER2_MIN,
+                0x8 => 0xA, // counter2 skips 0x9 rather than 0xF
+                c => c + 1,
+            })
+            .unwrap();
+        }
+
+        QueuedFrame::new(self.id(), self.raw())
+    }
+}
+
+impl PeriodicMessage for TractionControlMed {
+    fn rate(&self) -> Rate {
+        50.Hz()
+    }
+
+    fn update_for_transmit(&mut self, car: &CarState) -> Option<QueuedFrame> {
+        if car.ignition() != Ignition::On {
+            return None;  // IEB is only on when car is on
+        }
+
+        // Update brake pedal state
+        self.set_driver_braking(car.is_braking()).unwrap();
+
+        // Increment counter
+        {
+            self.set_counter(match self.counter() {
+                Self::COUNTER_MAX.. => Self::COUNTER_MIN,
+                n => n + 1,
+            })
+            .unwrap();
+        }
+
+        // Update checksum
+        {
+            self.set_checksum(0).unwrap();
+
+            // Checksum is the sum of the nibbles of all bytes except the last
+            let new_sum = self.raw()[..7]
+                .iter()
+                .fold(0u8, |n, e| (n + (e >> 4) + (e & 0xF)) & 0xF);
+
+            // Then take binary complement, add one, and mask modulo 16
+            self.set_checksum(((new_sum ^ 0x0F) + 1) & 0x0F).unwrap();
+        }
+
+        QueuedFrame::new(self.id(), self.raw())
+    }
+}
+
+// Brake pedal data. Includes pedal force field and other brake-proportional field.
+impl PeriodicMessage for Ieb2a2 {
+    fn rate(&self) -> Rate {
+        100.Hz()
+    }
+
+    fn update_for_transmit(&mut self, car: &CarState) -> Option<QueuedFrame> {
+        {
+            if car.ignition() != Ignition::On {
+                return None;  // IEB is only on when car is on
             }
 
-            // Brake pedal data. Includes pedal force field and other brake-proportional field.
-            {
-                brake_pedal
-                    .set_heart_beat(!brake_pedal.heart_beat())
-                    .unwrap();
+            self.set_heart_beat(!self.heart_beat()).unwrap();
 
-                if braking {
-                    brake_pedal.set_brake_pedal_force(0x101C).unwrap(); // Arbitrary value
-                    brake_pedal.set_brake_unknown(0x5e).unwrap(); // BrakeUnknown, roughly correlates with BrakePedalForce
-                } else {
-                    brake_pedal.set_brake_pedal_force(0).unwrap();
-                    brake_pedal.set_brake_unknown(0).unwrap();
+            if car.is_braking() {
+                self.set_brake_pedal_force(0x101C).unwrap(); // Arbitrary value
+                self.set_brake_unknown(0x5e).unwrap(); // BrakeUnknown, roughly correlates with BrakePedalForce
+            } else {
+                self.set_brake_pedal_force(0).unwrap();
+                self.set_brake_unknown(0).unwrap();
+            }
+        }
+
+        QueuedFrame::new(self.id(), self.raw())
+    }
+}
+
+impl PeriodicMessage for Ieb331 {
+    fn rate(&self) -> Rate {
+        100.Hz()
+    }
+
+    fn update_for_transmit(&mut self, car: &CarState) -> Option<QueuedFrame> {
+        if car.ignition() != Ignition::On {
+            return None;  // IEB is only on when car is on
+        }
+
+        // Seems to be approx 2x "BrakeUnknown" in Ieb2a2
+        self.set_brake_unknown(if car.is_braking() { 0xEB } else { 0x00 })
+            .unwrap();
+
+        QueuedFrame::new(self.id(), self.raw())
+    }
+}
+
+// IEB 386, wheel speed data
+impl PeriodicMessage for Ieb386Wheel {
+    fn rate(&self) -> Rate {
+        50.Hz()
+    }
+
+    fn update_for_transmit(&mut self, car: &CarState) -> Option<QueuedFrame> {
+        if car.ignition() != Ignition::On {
+            return None;  // IEB is only on when car is on
+        }
+
+        // Live counters in the top 2 bits of each 16-bit wheel speed value
+        self.set_whl_spd_alive_counter_lsb(match self.whl_spd_alive_counter_lsb() {
+            Ieb386Wheel::WHL_SPD_ALIVE_COUNTER_LSB_MAX.. => {
+                Ieb386Wheel::WHL_SPD_ALIVE_COUNTER_LSB_MIN
+            }
+            lsb => lsb + 1,
+        })
+        .unwrap();
+
+        if self.whl_spd_alive_counter_lsb() == Ieb386Wheel::WHL_SPD_ALIVE_COUNTER_LSB_MIN {
+            // When LSB wraps, increment the MSB
+            self.set_whl_spd_alive_counter_msb(match self.whl_spd_alive_counter_msb() {
+                Ieb386Wheel::WHL_SPD_ALIVE_COUNTER_MSB_MAX.. => {
+                    Ieb386Wheel::WHL_SPD_ALIVE_COUNTER_MSB_MIN
                 }
-            }
-
-            // IEB 331, unknown message includes wheel speed data
-            {
-                // Seems to be approx 2x "BrakeUnknown" in brake_pedal
-                ieb_331
-                    .set_brake_unknown(if braking { 0xEB } else { 0x00 })
-                    .unwrap();
-            }
-
-            // ESP status
-            {
-                stability.update();
-            }
-
-            pcan_tx.lock(|tx| {
-                // TODO: can be a loop or a map, maybe?
-                tx.transmit(&traction_fast);
-                tx.transmit(&brake_pedal);
-                tx.transmit(&ieb_331);
-                tx.transmit(&stability);
-            });
+                msb => msb + 1,
+            })
+            .unwrap();
         }
 
-        if every_50hz.due(&group) {
-            // IEB 386, wheel speed data
-            {
-                // Live counters in the top 2 bits of each 16-bit wheel speed value
-                ieb_386
-                    .set_whl_spd_alive_counter_lsb(match ieb_386.whl_spd_alive_counter_lsb() {
-                        Ieb386Wheel::WHL_SPD_ALIVE_COUNTER_LSB_MAX.. => {
-                            Ieb386Wheel::WHL_SPD_ALIVE_COUNTER_LSB_MIN
-                        }
-                        lsb => lsb + 1,
-                    })
-                    .unwrap();
+        // Note: the rear wheel speeds have 2-bit checksum fields (rather than live counters), but
+        // if the rear wheel speeds stay at 0 then these checksums don't change either (are set in the
+        // hex string for the constructor.)
 
-                if ieb_386.whl_spd_alive_counter_lsb() == Ieb386Wheel::WHL_SPD_ALIVE_COUNTER_LSB_MIN
-                {
-                    // When LSB wraps, increment the MSB
-                    ieb_386
-                        .set_whl_spd_alive_counter_msb(match ieb_386.whl_spd_alive_counter_msb() {
-                            Ieb386Wheel::WHL_SPD_ALIVE_COUNTER_MSB_MAX.. => {
-                                Ieb386Wheel::WHL_SPD_ALIVE_COUNTER_MSB_MIN
-                            }
-                            msb => msb + 1,
-                        })
-                        .unwrap();
-                }
+        QueuedFrame::new(self.id(), self.raw())
+    }
+}
 
-                // Note: the rear wheel speeds have 2-bit checksum fields (rather than live counters), but
-                // if the rear wheel speeds stay at 0 then these checksums don't change either (are set in the
-                // hex string for the constructor.)
-            }
+// IEB 387 Wheel pulse counts
+impl PeriodicMessage for Ieb387Wheel {
+    fn rate(&self) -> Rate {
+        50.Hz()
+    }
 
-            // IEB 387 Wheel pulse counts
-            {
-                ieb_387
-                    .set_alive_counter_whl_pul(match ieb_387.alive_counter_whl_pul() {
-                        Ieb387Wheel::ALIVE_COUNTER_WHL_PUL_MAX.. => {
-                            Ieb387Wheel::ALIVE_COUNTER_WHL_PUL_MIN
-                        }
-                        n => n + 1,
-                    })
-                    .unwrap();
-
-                // Byte 5 is a checksum that weirdly includes byte 6 after it, maybe also 7
-                ieb_387.set_whl_pul_chksum(0).unwrap();
-                ieb_387
-                    .set_whl_pul_chksum(byte_checksum_simple(ieb_387.raw()))
-                    .unwrap();
-            }
-
-            // Traction Control Medium Speed Message (0x394)
-            {
-                traction_med.set_driver_braking(braking).unwrap();
-                traction_med.update();
-            }
-
-            pcan_tx.lock(|tx| {
-                tx.transmit(&ieb_386);
-                tx.transmit(&ieb_387);
-                tx.transmit(&traction_med);
-            });
+    fn update_for_transmit(&mut self, car: &CarState) -> Option<QueuedFrame> {
+        if car.ignition() != Ignition::On {
+            return None;  // IEB is only on when car is on
         }
 
-        if every_20hz.due(&group) {
-            pcan_tx.lock(|tx| {
-                tx.transmit(&parking_brake);
-            });
+        // Update counter
+        {
+            self.set_alive_counter_whl_pul(match self.alive_counter_whl_pul() {
+                Ieb387Wheel::ALIVE_COUNTER_WHL_PUL_MAX.. => Ieb387Wheel::ALIVE_COUNTER_WHL_PUL_MIN,
+                n => n + 1,
+            })
+            .unwrap();
         }
 
-        if every_10hz.due(&group) {
-            pcan_tx.lock(|tx| {
-                tx.transmit(&ieb_507);
-            });
+        // Update checksum
+        {
+            // Byte 5 is a checksum that weirdly includes byte 6 after it, maybe also 7
+            self.set_whl_pul_chksum(0).unwrap();
+            self.set_whl_pul_chksum(byte_checksum_simple(self.raw()))
+                .unwrap();
         }
+
+        QueuedFrame::new(self.id(), self.raw())
+    }
+}
+
+impl PeriodicMessage for Ieb507Tcs {
+    fn rate(&self) -> Rate {
+        10.Hz()
+    }
+
+    fn update_for_transmit(&mut self, car: &CarState) -> Option<QueuedFrame> {
+        if car.ignition() != Ignition::On {
+            return None;  // IEB is only on when car is on
+        }
+
+        QueuedFrame::new(self.id(), self.raw())
+    }
+}
+
+impl PeriodicMessage for ParkingBrake {
+    fn rate(&self) -> Rate {
+        20.Hz()
+    }
+
+    fn update_for_transmit(&mut self, car: &CarState) -> Option<QueuedFrame> {
+        if car.ignition() != Ignition::On {
+            return None;  // IEB is only on when car is on
+        }
+
+        QueuedFrame::new(self.id(), self.raw())
+    }
+}
+
+impl PeriodicMessage for StabilityControl {
+    fn rate(&self) -> Rate {
+        100.Hz()
+    }
+
+    fn update_for_transmit(&mut self, car: &CarState) -> Option<QueuedFrame> {
+        if car.ignition() != Ignition::On {
+            return None;  // IEB is only on when car is on
+        }
+
+        // Increment counter
+        {
+            self.set_counter(match self.counter() {
+                Self::COUNTER_MAX.. => Self::COUNTER_MIN,
+                n => n + 1,
+            })
+            .unwrap();
+        }
+
+        // Update checksum
+        {
+            self.set_checksum(0).unwrap();
+            let new_sum = self.raw().iter().fold(0u8, |n, e| n.wrapping_add(*e));
+            self.set_checksum((new_sum ^ 0x9) & 0xF).unwrap();
+        }
+
+        QueuedFrame::new(self.id(), self.raw())
     }
 }
